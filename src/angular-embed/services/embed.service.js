a7c1b7521b08b8107d5bc12e8c6f8d46647054e2
Edouard <edou4rd@gmail.com>
2015-01-12 15:42:23 +0100

    comments

--- src/angular-embed/services/embed.service.js
+++ src/angular-embed/services/embed.service.js
@@ -4,6 +4,7 @@ function embedService(embedlyService, noEmbedService, $q) {
     var noEmbedProviders = noEmbedService.providers();
 
     function isSupportedByNoEmbedProviders(providers, url) {
+        // test the url with all the providers. Return true if the url match a provider
         return providers.some(function(provider) {
             return provider.patterns.some(function(pattern) {
                 var regex = new RegExp(pattern);
@@ -14,23 +15,32 @@ function embedService(embedlyService, noEmbedService, $q) {
 
     return {
         get: function(url) {
+            // prepare a promise to be returned quickly
             var deferred = $q.defer();
-            noEmbedProviders.then(function successCallback(providers) {
-                // if the url is in the NoEmbed providers list, we use NoEmbed
-                if (isSupportedByNoEmbedProviders(providers, url)) {
-                    deferred.resolve(noEmbedService.embed(url));
-                } else {
-                    // otherwise we use embedly which limits requests
-                    embedlyService.embed(url).then(function(response) {
-                        deferred.resolve(response.data);
-                    });
-                }
-            }, function errorCallback(error) {
-                // on NoembedProviders error, use the embedly service
+            // return the embedly response to the promise
+            function useEmbedlyService() {
                 embedlyService.embed(url).then(function(response) {
                     deferred.resolve(response.data);
                 });
+            }
+            // return the noEmbed response to the promise
+            function useNoEmbedService() {
+                deferred.resolve(noEmbedService.embed(url));
+            }
+            // wait the providers list
+            noEmbedProviders.then(function noEmbedProvidersSuccessCallback(providers) {
+                // if the url is in the NoEmbed providers list, we use NoEmbed
+                if (isSupportedByNoEmbedProviders(providers, url)) {
+                    useNoEmbedService();
+                } else {
+                    // otherwise we use embedly which limits requests
+                    useEmbedlyService();
+                }
+            }, function noEmbedProvidersErrorCallback(error) {
+                // on NoembedProviders error, use the embedly service
+                useEmbedlyService();
             });
+            // return the promise
             return deferred.promise;
         }
     };
