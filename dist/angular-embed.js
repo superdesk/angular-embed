3e0eb29de7e0d7db7528e218273b6bced2dd574a
Edouard <edou4rd@gmail.com>
2015-01-12 15:13:46 +0100

    handle errors

--- dist/angular-embed.js
+++ dist/angular-embed.js
@@ -39,14 +39,21 @@ function embedService(embedlyService, noEmbedService, $q) {
     return {
         get: function(url) {
             var deferred = $q.defer();
-            noEmbedProviders.then(function(providers){
+            noEmbedProviders.then(function successCallback(providers) {
+                // if the url is in the NoEmbed providers list, we use NoEmbed
                 if (isSupportedByNoEmbedProviders(providers, url)) {
                     deferred.resolve(noEmbedService.embed(url));
                 } else {
+                    // otherwise we use embedly which limits requests
                     embedlyService.embed(url).then(function(response) {
                         deferred.resolve(response.data);
                     });
                 }
+            }, function errorCallback(error) {
+                // on NoembedProviders error, use the embedly service
+                embedlyService.embed(url).then(function(response) {
+                    deferred.resolve(response.data);
+                });
             });
             return deferred.promise;
         }
